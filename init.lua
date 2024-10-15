660fbbbad85133f9a797eb7ed69106b864185d71
Matti Pulkkinen <133065141+mattipulkkinen@users.noreply.github.com>
2024-10-15 19:02:13 +0300

    Enable LSP for javascript

--- init.lua
+++ init.lua
@@ -615,6 +615,7 @@ require('lazy').setup({
         -- pyright = {},
         rust_analyzer = {},
         basedpyright = {},
+        ts_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
